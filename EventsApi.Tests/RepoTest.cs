95e07161aa468d6a5fe80582f07fd7e84da07b36
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-03-21 13:11:09 +0100

    Error_removed

---
+++ EventsApi.Tests/RepoTest.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using EventsApi.Repos;
+using EventsApi.Models;
+namespace EventsApi.Tests
+{
+    public class RepoTest
+    {
+        [Fact]
+        public void RepoList_WithSeedData_IsNotEmpty()
+        {
+            // Arrange & Act
+            var repo = new RepoList<Event>(includeData: true);
+            var events = repo.GetAll();
+
+            // Assert
+            Assert.NotEmpty(events);
+        }
+
+        [Fact]
+        public void RepoList_FilterByCategory_ReturnsOnlyMatchingEvents()
+        {
+            // Arrange
+            var repo = new RepoList<Event>(includeData: true);
+
+            /// Act
+            var result = repo.GetAll(category: "Music").ToList();
+
+            /// Assert
+            Assert.NotEmpty(result);
+            Assert.All(result, e =>
+                Assert.Contains("Music", e.Category, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
